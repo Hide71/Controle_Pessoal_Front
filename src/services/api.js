20b2ab7db8085703a64ae5a898215181ee2119f7
Hide71 <hidera1@hotmail.com>
2025-03-27 00:01:26 -0300

    ajuste nome do item localStorage e interpolação do access token

--- src/services/api.js
+++ src/services/api.js
@@ -1,31 +1,32 @@
-import axios from "axios"
-import { configs } from "eslint-plugin-react-refresh"
+import axios from "axios";
+import { configs } from "eslint-plugin-react-refresh";
 
 const api = axios.create({
-    baseURL: "http://localhost:5119",
-})
-api.interceptors.request.use((config) =>{
-    const token = localStorage.getItem('acess_token')
-    if(token){
-        config.headers['Authorization'] = 'Bearer ${token}'
+  baseURL: "http://localhost:5119",
+});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
-    return config
-},
- (error) =>{
-    return Promise.reject(error)
-}
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 api.interceptors.response.use(
-    (response) => {
-      return response
-    },
-    (error) => {
-      if (error.response && error.response.status === 401) {
-        console.error('Token expirado ou inválido.')
-        localStorage.removeItem('access_token')
-        window.location.href = '/'
-      }
-      return Promise.reject(error)
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.error("Token expirado ou inválido.");
+      localStorage.removeItem("access_token");
+      window.location.href = "/";
     }
-  )
-export default api
+    return Promise.reject(error);
+  }
+);
+export default api;
