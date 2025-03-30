838c377f6811fd8ef210c11b86c21a9f5ae3016b
Hide71 <hidera1@hotmail.com>
2025-03-30 17:05:16 -0300

    Login/Cadastro com o Google

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,67 +1,69 @@
-import { userLogin } from "../../services/userContoller"
-import { GoogleLogin } from '@react-oauth/google';
-import { useState } from 'react'
-import { Link , useNavigate} from 'react-router-dom'
-import './style.css'
-import ErrorBoundary from '../../services/ErrorBoundary'
+import { userLogin } from "../../services/userContoller";
+import { GoogleLogin } from "@react-oauth/google";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./style.css";
+import ErrorBoundary from "../../services/ErrorBoundary";
 
 function Login() {
-  const [userName, setUserName] = useState('')
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if ( !userName || !email || !password) {
-      setError('Por favor, preencha todos os campos.');
+    if (!userName || !email || !password) {
+      setError("Por favor, preencha todos os campos.");
       return;
     }
 
     setIsLoading(true);
-    setError('');
+    setError("");
 
     try {
-      const data = await userLogin({userName, email, password });
+      const data = await userLogin({ userName, email, password });
 
       if (data.access_token) {
-        localStorage.setItem('access_token', data.access_token);
-        alert("Sucesso!!")
-        navigate('/despesa');
+        localStorage.setItem("access_token", data.access_token);
+        alert("Sucesso!!");
+        navigate("/despesa");
       } else {
-        setError('Usuário, Email ou senha inválidos.');
+        setError("Usuário, Email ou senha inválidos.");
       }
     } catch (error) {
-      console.error('Erro ao tentar fazer login:', error);
-      setError('Erro ao tentar fazer login, usuário não está cadastrado.');
+      console.error("Erro ao tentar fazer login:", error);
+      setError("Erro ao tentar fazer login, usuário não está cadastrado.");
     } finally {
       setIsLoading(false);
     }
-  }
+  };
   const handleGoogleLoginSuccess = async (response) => {
     try {
       const { credential } = response; // O token do Google
-      const result = await userLogin({ googleToken: credential });
+      const result = await userLogin({
+        email: "googleAuth",
+        password: credential,
+      });
 
       if (result.access_token) {
-        localStorage.setItem('access_token', result.access_token);
-        navigate('/despesa');
+        localStorage.setItem("access_token", result.access_token);
+        navigate("/despesa");
       } else {
-        setError('Erro ao autenticar com o Google.');
+        setError("Erro ao autenticar com o Google.");
       }
     } catch (error) {
-      console.error('Erro ao tentar fazer login com o Google:', error);
-      setError('Erro ao tentar fazer login com o Google.');
+      console.error("Erro ao tentar fazer login com o Google:", error);
+      setError("Erro ao tentar fazer login com o Google.");
     }
-  }
+  };
   const handleGoogleLoginFailure = (error) => {
-    console.error('Erro no login com o Google:', error);
-    setError(`Erro no Google Login: ${error.message || 'Erro desconhecido'}`);
-  }
-  
+    console.error("Erro no login com o Google:", error);
+    setError(`Erro no Google Login: ${error.message || "Erro desconhecido"}`);
+  };
 
   return (
     <div className="center-div">
@@ -93,7 +95,7 @@ function Login() {
             />
           </div>
           <button type="submit" disabled={isLoading}>
-            {isLoading ? 'Entrando...' : 'Entrar'}
+            {isLoading ? "Entrando..." : "Entrar"}
           </button>
           <div className="signup-link">
             <p>
@@ -113,7 +115,7 @@ function Login() {
             </div>
           </div>
         </form>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </div>
     </div>
   );
