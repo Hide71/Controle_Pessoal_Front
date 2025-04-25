0ecc2444b63db7746ab7317c6f5c023d738272aa
Hide71 <hidera1@hotmail.com>
2025-04-25 14:44:47 -0300

    criando botão logout

--- src/pages/Login/userProfile.jsx
+++ src/pages/Login/userProfile.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { jwtDecode } from "jwt-decode";
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const UserProfile = () => {
-  // Estado para armazenar o nome e a foto do usuário
   const [userPicture, setUserPicture] = useState('');
   const [userName, setUserName] = useState('');
   const [userEmail, setUserEmail] = useState('');
@@ -11,22 +10,18 @@ const UserProfile = () => {
 
   const handleLogout = () => {
     localStorage.removeItem('access_token');
-    navigate("/")
+    navigate("/");
   };
 
   useEffect(() => {
-    // Recupera o access_token do localStorage
     const token = localStorage.getItem('access_token');
 
     if (token) {
       try {
-        // Decodifica o token JWT
         const decoded = jwtDecode(token);
-
-        // Extrai o nome e a foto do usuário do payload
         setUserPicture(decoded.picture);
         setUserName(decoded.name);
-        setUserEmail(decoded.email)
+        setUserEmail(decoded.email);
       } catch (error) {
         console.error('Erro ao decodificar o token:', error);
       }
@@ -36,19 +31,32 @@ const UserProfile = () => {
   }, []);
 
   return (
-    <div>
+    <div style={{ textAlign: "left", marginTop: '20px' }}>
       {userPicture ? (
-        <img 
-          src={userPicture} 
-          alt="Foto de perfil do usuário" 
-          style={{ width: '50px', height: '50px', borderRadius: '50%' }} 
-          onClick={handleLogout}
+        <img
+          src={userPicture}
+          alt="Foto de perfil do usuário"
+          style={{ width: '50px', height: '50px', borderRadius: '50%', marginBottom: '10px' }}
         />
       ) : (
         <div>Carregando foto...</div>
       )}
       <h2>{userName || 'Nome do Usuário'}</h2>
-      <h2>{userEmail || 'email do Usuário'}</h2>
+      <p>{userEmail || 'Email do Usuário'}</p>
+      <button 
+        onClick={handleLogout} 
+        style={{
+          marginTop: '15px',
+          padding: '10px 20px',
+          backgroundColor: '#e63946',
+          color: 'white',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer'
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
